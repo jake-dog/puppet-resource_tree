f29fbd618b2f81a779324fe9c65cec919d0e25ec
Jake Comer <jake-dog@users.noreply.github.com>
2015-01-31 12:33:16 -0500

    adding ability to require other elements of resource tree

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class resource_tree (
   $allcollections = hiera_hash('resource_tree::collections', $collections)
   
   if $allcollections {
-      $uniq_resources = parseyaml(inline_template('<%= Hash[@allcollections.values().inject({}) {|a,b| a.merge(b) }.map {|k,v| Hash[v.map {|a,b| [k+"-"+a,{"type" => k, "resources" => b.fetch("resources",nil),"params" => {a=>b.reject {|x,y| x=="resources"}}}]}] }.flatten(1).inject({}) {|a,b| a.merge(b) }.to_yaml %>'))
+      $uniq_resources = parseyaml(inline_template('<%= Hash[@allcollections.values().inject({}) {|a,b| a.merge(b) }.map {|k,v| Hash[v.map {|a,b| [k+"-"+a,{"type" => k, "rt_requires" => b.fetch("rt_requires",[]), "resources" => b.fetch("resources",nil),"params" => {a=>b.reject {|x,y| ["resources", "rt_requires"].include? x }}}]}] }.flatten(1).inject({}) {|a,b| a.merge(b) }.to_yaml %>'))
       create_resources('resource_tree::resource', $uniq_resources, {})
   }
 }
--- manifests/resource.pp
+++ manifests/resource.pp
@@ -1,14 +1,20 @@
 define resource_tree::resource (
     type,
     params,
+    rt_requires = nil,
     resources = nil,
 ) {
   
   if $resources {
-      $uniq_resources = parseyaml(inline_template('<%= Hash[@resources.map {|k,v| Hash[v.map {|a,b| [k+"-"+a,{"type" => k, "resources" => b.fetch("resources",nil),"params" => {a=>b.reject {|x,y| x=="resources"}}}]}] }.flatten(1).inject({}) {|a,b| a.merge(b) }.to_yaml %>'))
+      $uniq_resources = parseyaml(inline_template('<%= Hash[@resources.map {|k,v| Hash[v.map {|a,b| [k+"-"+a,{"type" => k, "rt_requires" => b.fetch("rt_requires",[]), "resources" => b.fetch("resources",nil),"params" => {a=>b.reject {|x,y| ["resources", "rt_requires"].include? x }}}]}] }.flatten(1).inject({}) {|a,b| a.merge(b) }.to_yaml %>'))
       create_resources('resource_tree::resource', $uniq_resources, { 'require' => Resource_tree::Marker[$name] })
   }
 
   resource_tree::marker{ "$name": }
-  create_resources(type, params, { 'before' => Resource_tree::Marker[$name] })
+  
+  if $rt_requires {
+    create_resources(type, params, { 'before' => Resource_tree::Marker[$name], 'require' => Resource_tree::Marker[$rt_requires] })
+  } else {
+    create_resources(type, params, { 'before' => Resource_tree::Marker[$name] })
+  }
 }
