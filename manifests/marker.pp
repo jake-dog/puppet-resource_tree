9bac294b89a241a5fa494707730c5ff4e0691314
Jake Comer <jake-dog@users.noreply.github.com>
2015-01-31 12:36:04 -0500

    renaming marker as placeholder

--- manifests/marker.pp
+++
@@ -1,1 +0,0 @@
-define resource_tree::marker () { }
--- manifests/resource.pp
+++ manifests/resource.pp
@@ -7,14 +7,14 @@ define resource_tree::resource (
   
   if $resources {
       $uniq_resources = parseyaml(inline_template('<%= Hash[@resources.map {|k,v| Hash[v.map {|a,b| [k+"-"+a,{"type" => k, "rt_requires" => b.fetch("rt_requires",[]), "resources" => b.fetch("resources",nil),"params" => {a=>b.reject {|x,y| ["resources", "rt_requires"].include? x }}}]}] }.flatten(1).inject({}) {|a,b| a.merge(b) }.to_yaml %>'))
-      create_resources('resource_tree::resource', $uniq_resources, { 'require' => Resource_tree::Marker[$name] })
+      create_resources('resource_tree::resource', $uniq_resources, { 'require' => Resource_tree::Placeholder[$name] })
   }
 
-  resource_tree::marker{ "$name": }
+  resource_tree::placeholder{ "$name": }
   
   if $rt_requires {
-    create_resources(type, params, { 'before' => Resource_tree::Marker[$name], 'require' => Resource_tree::Marker[$rt_requires] })
+    create_resources(type, params, { 'before' => Resource_tree::Placeholder[$name], 'require' => Resource_tree::Placeholder[$rt_requires] })
   } else {
-    create_resources(type, params, { 'before' => Resource_tree::Marker[$name] })
+    create_resources(type, params, { 'before' => Resource_tree::Placeholder[$name] })
   }
 }
