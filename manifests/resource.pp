5c8ddfec4faf6ab3ce569b70ea775eba1b05f8c2
Jake Comer <jake-dog@users.noreply.github.com>
2016-10-17 11:29:30 -0400

    ignoring "before" requirement when "type" is exported

--- manifests/resource.pp
+++ manifests/resource.pp
@@ -12,7 +12,13 @@ define resource_tree::resource (
       create_resources('resource_tree::resource', $uniq_resources, { 'require' => Resource_tree::Placeholder[$name] })
   }
 
-  resource_tree::placeholder{ $name: }
+  # Ignore "before" requirement when "type" is exported
+  if $type.match(/^[@][@]/) {
+    $before = []
+  } else {
+    resource_tree::placeholder{ "$name": }
+    $before = [Resource_tree::Placeholder[$name]]
+  }
   
   # Allow arbitrary commands and nested yaml
   $parsed_params = parseyaml(inline_template('<%= Hash[@params.keys[0], Hash[@params.values[0].map {|k,v| ((k.start_with? "rt_parse::") && [k[10..-1], YAML.load((((v.respond_to? :start_with?) && (v.start_with? "rt_eval::")) && eval(v[9..-1])) || v)]) || [k,(((v.respond_to? :start_with?) && (v.start_with? "rt_eval::")) && eval(v[9..-1])) || v] }]].to_yaml %>'))
@@ -39,15 +45,15 @@ define resource_tree::resource (
     $all_notify = concat(concat($exec_notify,$mount_notify),$service_notify)
     
     if $rt_requires {
-      create_resources($type, $parsed_params, { 'before' => Resource_tree::Placeholder[$name], 'require' => Resource_tree::Placeholder[$rt_requires], 'notify' => $all_notify })
+      create_resources($type, $parsed_params, { 'before' => $before, 'require' => Resource_tree::Placeholder[$rt_requires], 'notify' => $all_notify })
     } else {
-      create_resources($type, $parsed_params, { 'before' => Resource_tree::Placeholder[$name], 'notify' => $all_notify })
+      create_resources($type, $parsed_params, { 'before' => $before, 'notify' => $all_notify })
     }
   } else {
     if $rt_requires {
-      create_resources($type, $parsed_params, { 'before' => Resource_tree::Placeholder[$name], 'require' => Resource_tree::Placeholder[$rt_requires] })
+      create_resources($type, $parsed_params, { 'before' => $before, 'require' => Resource_tree::Placeholder[$rt_requires] })
     } else {
-      create_resources($type, $parsed_params, { 'before' => Resource_tree::Placeholder[$name] })
+      create_resources($type, $parsed_params, { 'before' => $before })
     }
   }
 }
