3f98a21fe90daccf11ccbf281de823538b000b22
Jake Comer <jake-dog@users.noreply.github.com>
2016-11-06 16:47:11 -0500

    attempting to add support for default params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,11 @@
 class resource_tree (
   $collections    = {},
   $apply          = [],
+  $default_params = {},
 ) {
   $allcollections = hiera_hash('resource_tree::collections', $collections)
   $allapply = hiera_array('resource_tree::apply', $apply)
+  $defaults = hiera_hash('resource_tree::default_params', $default_params)
   
   if is_hash($allcollections) and is_array($allapply) and (size(intersection($allapply, keys($allcollections))) > 0) {
       $uniq_resources = parseyaml(template('resource_tree/main.erb'))
