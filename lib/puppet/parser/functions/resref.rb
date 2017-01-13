314c3603730e63a914904e37aa39deb3676d9e5c
Jake Comer <jake-dog@users.noreply.github.com>
2017-01-12 19:41:33 -0500

    hashes shouldn't be given special treatment

--- lib/puppet/parser/functions/resref.rb
+++ lib/puppet/parser/functions/resref.rb
@@ -20,6 +20,11 @@ aforementioned method is faster and easier to read.
 
 Read more about puppet Type references here:
 https://docs.puppet.com/puppet/latest/lang_data_resource_type.html#examples-1
+
+*Examples:*
+    resref('file-/foo/bar', 'File[/tmp]', {'file' => '/var'})
+Would return:
+    [File["/foo/bar"], File["/tmp"], File["/var"]]
 ENDOFDOC
 ) do |args|
   # our basic string resource reference structure
@@ -38,24 +43,22 @@ ENDOFDOC
 
   # convert all args to list of RefString or resource string
   args.map{|res|
-    if res.is_a? (Hash)
-      # hash could include array or string args so we join them
-      res.map{|k,v|
-        v.is_a?(Array)? v.map{|e| RefString.new(k,e) } : RefString.new(k,v)
-      }.flatten
-    else
-      # Convert whatever remains into an array
-      (res.is_a?(Array)? res : [res]).map{|e|
-        # regex is straight from puppetlabs, but with a dash at the end
-        # https://docs.puppet.com/puppet/latest/lang_reserved.html#classes-and-defined-resource-types
-        if e =~ /\A([a-z][a-z0-9_]*)?(::[a-z][a-z0-9_]*)*-/
-          RefString.new(e.to_s.split("-")[0].capitalize, e.to_s.split("-")[1..-1].join("-"))
-        elsif e.is_a?(Array)
-          RefString.new(e[0].to_s.capitalize, e[1].to_s)
-        else
-          e
-        end
-      }
-    end
+    # Convert each argument into an array
+    (res.is_a?(Array)? res : [res]).map{|e|
+      # regex is straight from puppetlabs, but with a dash at the end
+      # https://docs.puppet.com/puppet/latest/lang_reserved.html#classes-and-defined-resource-types
+      if e =~ /\A([a-z][a-z0-9_]*)?(::[a-z][a-z0-9_]*)*-/
+        RefString.new(e.to_s.split("-")[0].capitalize, e.to_s.split("-")[1..-1].join("-"))
+      elsif e.is_a? (Hash)
+        # hash could include array or string args so we join them
+        e.map{|k,v|
+          v.is_a?(Array)? v.map{|o| RefString.new(k,o) } : RefString.new(k,v)
+        }.flatten
+      elsif e.is_a?(Array)
+        RefString.new(e[0].to_s.capitalize, e[1].to_s)
+      else
+        e
+      end
+    }
   }.flatten.map {|refstring| str2res.call(refstring) }
 end
