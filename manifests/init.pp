4d3b3f28985c246b67fd727df4dee5d18f7fcccf
Jake Comer <jake-dog@users.noreply.github.com>
2017-01-16 17:29:25 -0500

    making the silly puppet parser happy

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@ class resource_tree (
   $allcollections = hiera_hash('resource_tree::collections', $collections)
   $allapply = hiera_array('resource_tree::apply', $apply)
   $defaults = hiera_hash('resource_tree::default_params', $default_params)
-  
+
   if is_hash($allcollections) and is_array($allapply) and (size(intersection($allapply, keys($allcollections))) > 0) {
       $uniq_resources = parseyaml(template('resource_tree/main.erb'))
       create_resources('resource_tree::resource', $uniq_resources, {})
--- manifests/resource.pp
+++ manifests/resource.pp
@@ -8,12 +8,12 @@ define resource_tree::resource (
     $rt_resources   = undef,
 ) {
   include stdlib
-  
+
   if $rt_resources {
       $uniq_resources = parseyaml(template('resource_tree/resource.erb'))
       create_resources('resource_tree::resource', $uniq_resources)
   }
-  
+
   # Allow arbitrary commands and nested yaml
   $parsed_params = parseyaml(template('resource_tree/param_parse.erb'))
 
@@ -39,7 +39,7 @@ define resource_tree::resource (
     $require = []
   }
 
-  create_resources($type, $parsed_params,
-                   { 'require' => $require, 'notify' => $notify,
-                     'subscribe' => $subscribe, 'before' => $before })
+  create_resources($type, $parsed_params, {
+    'require' => $require, 'notify' => $notify,
+    'subscribe' => $subscribe, 'before' => $before })
 }
